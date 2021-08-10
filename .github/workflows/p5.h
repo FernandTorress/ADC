aa898667f57f6aece40db2db3e271dc70826e143
FernandTorress <77314143+FernandTorress@users.noreply.github.com>
2021-08-09 21:10:47 -0500

    Create p5.h

---
+++ .github/workflows/p5.h
@@ -0,0 +1,12 @@
+#ifndef P5_H_
+#define P5_H_
+
+#define OUTPUT 1
+#define INPUT 0
+#define ON 1
+#define OFF 0
+
+	void pinMode(unsigned char puerto, int bit, unsigned char mode);
+	void digitalWrite(unsigned char puerto, int bit, unsigned char data);
+
+#endif /* P5_H_ */
